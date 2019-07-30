86ad88b7b0bcd9afa9aefe2a4ef587d1c18549fa
Daniel <ddan62626@gmail.com>
2019-07-31 02:36:30 +0300

    Add files via upload

--- Underfoot/server.js
+++ Underfoot/server.js
@@ -8,7 +8,7 @@ var SHA256 = require('./script/module/build/SHA256.min.js');
  
 
 
-var whitelist = ['http://localhost:3000'];
+var whitelist = ['http://localhost:3000','https://glacial-hamlet-30025.herokuapp.com'];
 var corsOptionsDelegate = function (req, callback) {
   var corsOptions;
   if (whitelist.indexOf(req.header('Origin')) !== -1) {
